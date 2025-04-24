279557c3042581c59f7cc626d1a4218f0307247a
ASWIN KUMAR S <saswinvijay77@gmail.com>
2025-04-24 16:39:31 +0530

    Update 8A.c
    3

--- 8A.c
+++ 8A.c
@@ -5,6 +5,9 @@ printf("Value of a is %d\n", a);
 }
 int main()
 {
-void (*fun_ptr)(int) = &fun; void (*fun_ptr)(int); fun_ptr = &fun; (*fun_ptr)(10);
+void (*fun_ptr)(int) = &fun; 
+  void (*fun_ptr)(int); 
+  fun_ptr = &fun; 
+  (*fun_ptr)(10);
 return 0;
 }
